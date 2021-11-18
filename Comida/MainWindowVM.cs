da80d6fb08d78f8fbb296fbb1c305253e319b3c3
Ruben9911 <ruben.fortes-marques@iesdoctorbalmis.com>
2021-11-18 12:31:06 +0100

    Agregar archivos de proyecto.

---
+++ Comida/MainWindowVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comida
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        private ObservableCollection<Plato> listaPlatos;
+
+        public ObservableCollection<Plato> ListaPlatos
+        {
+            get { return listaPlatos; }
+            set { listaPlatos = value;
+                NotifyPropertyChanged("ListaPlatos");
+            }
+        }
+        public MainWindowVM()
+        {
+            listaPlatos = Plato.GetSamples("assets/");
+        }
+
+        //Implementación de la interfaz INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
